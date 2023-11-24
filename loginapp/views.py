3fb28eafcb1d8aeebedc1cc76789fa6d9e7d2531
PrincePandeycode <pandeyprince4723@gmail.com>
2023-11-25 00:53:46 +0530

    signup form  is created

--- loginapp/views.py
+++ loginapp/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("hello")
+    return  render(request,"loginapp/index.html")
 
 def signIn(request):
     return render(request,"loginapp/signIn.html")
 
-def signout(request):
-    return render(request,"loginapp/signout.html")
-
 def signup(request):
     return render(request,"loginapp/signup.html")
+
+def signout(request):
+    pass
+
