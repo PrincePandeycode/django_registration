d724263b85829ac123308663ee716dde175fea65
PrincePandeycode <pandeyprince4723@gmail.com>
2023-11-25 01:18:34 +0530

    my signup page is ready and is redirecting to signIn

--- loginapp/views.py
+++ loginapp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return  render(request,"loginapp/index.html")
@@ -8,6 +11,24 @@ def signIn(request):
     return render(request,"loginapp/signIn.html")
 
 def signup(request):
+    if request.method == 'POST':
+        # user_name = request.POST.get(user_name) or
+        user_name = request.POST['user_name']
+        first_name = request.POST['first_name']
+        email = request.POST['email']
+        last_name = request.POST['last_name']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        myuser = User.objects.create_user(user_name,email,password1)
+        myuser.first_name = first_name
+        myuser.last_name = last_name
+        myuser.save()
+
+        messages.success(request,"your account has been created")
+        return redirect('signIn')
+        
+
     return render(request,"loginapp/signup.html")
 
 def signout(request):
